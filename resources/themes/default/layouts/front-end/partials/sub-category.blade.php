71eb748a7a298154bca19edea48b1abe9fed19d1
Hassan-Abdullah5 <hassanabdullah5353@gmail.com>
2023-11-17 13:01:13 +0500

    some changes

---
+++ resources/themes/default/layouts/front-end/partials/sub-category.blade.php
@@ -0,0 +1,11 @@
+@include('layouts.front-end.partials.header')
+
+@include('layouts.front-end.sub-category.section1')
+@include('layouts.front-end.sub-category.section2')
+@include('layouts.front-end.sub-category.section3')
+@include('layouts.front-end.sub-category.section4')
+@include('layouts.front-end.sub-category.section5')
+
+
+
+
