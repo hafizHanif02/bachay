4510aadda52c61f7f4e6ee43b021c9fb4eb9f79f
Muhammad Hannan <sheikh.hannan06@gmail.com>
2023-11-25 14:46:17 +0500

    Minor Bug Fixing

--- resources/themes/default/layouts/front-end/home/section7.blade.php
+++ resources/themes/default/layouts/front-end/home/section7.blade.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center align-items-center">
 
         <div class="col-12 text-center mb-5 sub-banner2">
-            <img src="{{asset('storage/app/public/deal/'.$flash_deal->banner) }}" style="border-radius:30px; width: 100%; height:50vh" srcset="">
+            <img src="{{asset('storage/app/public/deal/'.$flash_deal->banner) }}" style="border-radius:30px; width: 100%; height:50vh; object-fit:cover;" srcset="">
 
         </div>
 
