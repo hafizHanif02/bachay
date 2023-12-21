d244ac9c47446c96ebe1aea39a951ff49b75f1db
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2023-12-21 13:25:46 +0500

    fixing

--- resources/themes/default/layouts/front-end/home/ProductSlider.blade.php
+++ resources/themes/default/layouts/front-end/home/ProductSlider.blade.php
@@ -13,65 +13,65 @@
     <div class="row">
         @foreach ($latest_products as $products)
             <div class="col-md-6 col-lg-3 mb-4">
-                <a class="text-decoration-none" href="{{ route('product-detail', $products->id) }}">
-                    <div class="sub-card rounded-3 p-4">
-                        <div class="card1">
-                            <div class="first-sec card1">
-                                <div class="image-container">
-                                    <div class="imgCon">
+                <div class="sub-card rounded-3 p-4">
+                    <div class="card1">
+                        <div class="first-sec card1">
+                            <div class="image-container">
+                                <div class="imgCon">
+                                    <a class="text-decoration-none" href="{{ route('product-detail', $products->id) }}">
                                         <img class="object-fit-cover rounded-3"
                                             src="{{ asset("storage/app/public/product/thumbnail/$products->thumbnail") }}"
                                             alt="" class="img-fluid" width="100%" height="100%">
-                                    </div>
-                                    <div class="sec-best-seller mt-3">
-                                        <p>Best Seller</p>
-                                    </div>
-                                    <div class="wish-list mt-3 me-2">
-                                        <button id="wishlist-btn" class="p-0 bg-transparent rounded-circle forBorder">
-                                            <i class="bi bi-heart text-danger"></i>
-                                            {{-- <i
-                            class="bi {{ in_array($product->id, $wishlistProducts) ? 'bi-heart-fill' : 'bi-heart' }} text-danger"></i> --}}
-                                        </button>
-                                    </div>
-                                    {{-- <div class="wish-list mt-3 me-2">
-                                <a href=""><img src="{{ asset('public/images/heart.svg') }}"
-                                        alt=""></a>
-                            </div> --}}
-                                    <p class="card-text mt-3" id="productDescription">
-                                        @if (strlen($products->name) <= 20)
-                                            {{ $products->name }}
-                                        @else
-                                            {{ substr($products->name, 0, 20) }}<span id="dots"> ....</span>
-                                        @endif
-                                    </p>
-                                    <div class="d-flex">
-                                        <h6 class="card-text price">Rs.
-                                            {{ $products->unit_price - ($products->unit_price * $products->discount) / 100 }}
-                                        </h6>
-                                        <p class="bg-primary rounded-pill ps-2 pe-2 ms-2 text-white units">141 Solds
-                                        </p>
-                                    </div>
-                                    <p class="card-text"><span class="discount">Rs. {{ $products->unit_price }}</span>
-                                        <span class="text-success">-{{ $products->discount }}% Off</span>
-                                    </p>
-                                    <div class="subdiv d-flex justify-content-between">
-                                        <span href="#">Standard Delivery</span>
-                                        {{-- @foreach ($products->reviews as $reviews) --}}
-                                        <p class="rounded-pill text-white">4.9
-                                            <img class="mb-1" src="{{ asset('public/images/star.svg') }}"
-                                                alt="">
-                                        </p>
-                                        {{-- @endforeach --}}
-
-                                        <h5 class="text-dark">({{ $products->reviews_count }})</h5>
-                                    </div>
+                                    </a>
+                                </div>
+                                <div class="sec-best-seller mt-3">
+                                    <p>Best Seller</p>
+                                </div>
+                                <div class="wish-list mt-3 me-2">
+                                    <button type="button" name="wishlist-button-{{ $products->id }}"
+                                        class="p-0 bg-transparent rounded-circle forBorder"
+                                        onclick="addToWishlist('{{ $products->id }}')">
+                                        <i id="heartIcon{{ $products->id }}"
+                                            class="bi {{ in_array($products->id, $wishlistProductsArray) ? 'bi-heart-fill' : 'bi-heart' }} text-danger"></i>
+                                    </button>
 
                                 </div>
+                                {{-- <div class="wish-list mt-3 me-2">
+                                    <a href=""><img src="{{ asset('public/images/heart.svg') }}"
+                                            alt=""></a>
+                                </div> --}}
+                                <p class="card-text mt-3" id="productDescription">
+                                    @if (strlen($products->name) <= 20)
+                                        {{ $products->name }}
+                                    @else
+                                        {{ substr($products->name, 0, 20) }}<span id="dots"> ....</span>
+                                    @endif
+                                </p>
+                                <div class="d-flex">
+                                    <h6 class="card-text price">Rs.
+                                        {{ $products->unit_price - ($products->unit_price * $products->discount) / 100 }}
+                                    </h6>
+                                    {{-- <p class="bg-primary rounded-pill ps-2 pe-2 ms-2 text-white units">141 Solds
+                                    </p> --}}
+                                </div>
+                                <p class="card-text"><span class="discount">Rs. {{ $products->unit_price }}</span>
+                                    <span class="text-success">-{{ $products->discount }}% Off</span>
+                                </p>
+                                <div class="subdiv d-flex justify-content-between">
+                                    <a href="#">Standard Delivery</a>
+                                    @foreach ($products->reviews as $reviews)
+                                        <p class="rounded-pill text-white">{{ $reviews }} <img
+                                                src="{{ asset('public/images/star.svg') }}" alt=""></p>
+                                    @endforeach
+
+                                    <h5>({{ $products->reviews_count }})</h5>
+                                </div>
+
                             </div>
                         </div>
-
                     </div>
-                </a>
+
+                </div>
             </div>
         @endforeach
         {{-- <div class="col-md-6 col-lg-3 mb-4">
@@ -380,23 +380,29 @@
 </div> --}}
 
 <script>
-    function addToWishlist(productId) {
-        $.ajax({
-            type: "POST",
-            url: "/add-to-wishlist",
-            data: {
-                productId: productId,
-            },
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            dataType: "json",
-            success: function(data) {
-                // Handle success
-            },
-            error: function(response) {
-                // Handle error
+  function addToWishlist(productId) {
+    $.ajax({
+        type: "POST",
+        url: "/add-to-wishlist",
+        data: {
+            productId: productId,
+        },
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        dataType: "json",
+        success: function(data, status, xhr) {
+            var heartIcon = $('#heartIcon' + productId);
+            if (xhr.status === 200) {
+                heartIcon.toggleClass('bi-heart bi-heart-fill');
+            } else if (xhr.status === 201) {
+                alert("Something went wrong");
             }
-        });
-    }
+        },
+        error: function(response) {
+            // Handle error
+        }
+    });
+}
+
 </script>
