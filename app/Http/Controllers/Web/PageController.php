42cfd9ef09d894d06f980ddad9758a11c41326f1
Ahmed Fazil <ahmed.fazil896@gmail.com>
2023-12-28 02:48:56 +0500

    Update PageController.php

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\BusinessSetting;
 use App\Model\HelpTopic;
 use App\Model\Category;
+use App\Model\Product;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
