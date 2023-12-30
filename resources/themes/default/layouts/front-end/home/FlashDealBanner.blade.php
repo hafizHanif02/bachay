5e5f4fc4c37ac4fbeab7038f4ab092dfab427014
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-12-30 17:20:30 +0500

    Update FlashDealBanner.blade.php

--- resources/themes/default/layouts/front-end/home/FlashDealBanner.blade.php
+++ resources/themes/default/layouts/front-end/home/FlashDealBanner.blade.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center align-items-center">
 
         <div class="col-12 text-center mb-5 sub-banner2">
-        @if($flash_deal != 0)
+        @if($flash_deal)
             <img src="{{asset('storage/app/public/deal/'.$flash_deal->banner) }}" style="border-radius:30px; width: 100%; height:50vh; object-fit:cover;" srcset="">
         @endif
         </div>
