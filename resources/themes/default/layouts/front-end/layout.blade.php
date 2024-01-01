148a82f7620d155304f8b1176ec9a9943b6b81a7
Ali Azeem Khan <azeem.alikhan777@gmail.com>
2024-01-01 10:52:56 +0500

    Reload

--- resources/themes/default/layouts/front-end/layout.blade.php
+++ resources/themes/default/layouts/front-end/layout.blade.php
@@ -245,9 +245,4 @@
         }, 3000);
     }
 </script>
-<script>
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        document.cookie = "desktopEnabled=true; path=/";
-        location.reload();
-    }
-    </script>
+
