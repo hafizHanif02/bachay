13a138696d3b2d2b24530ccb9107078d7adeb800
shahoon malik <151923050+Shahoon-Ahmed-Malik@users.noreply.github.com>
2024-01-01 10:37:52 +0500

    added cart

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -616,7 +616,19 @@ class CartController extends Controller
         // $brands = Brand::get();
         $colors = Color::whereIn('name', $color)->get();
         $pricefilter = ceil(Product::orderBy('unit_price', 'DESC')->value('unit_price') / 300);
-        return view(VIEW_FILE_NAMES['my-cart-added'], (compact('products', 'home_categories')));
+
+        if (Auth::guard('customer')->check()) {
+            $wishlistProducts = DB::table('wishlists')->where('customer_id', Auth::guard('customer')->user()->id)->pluck('product_id');
+
+            $wishlistProductsArray = $wishlistProducts->toArray();
+
+            $cartProducts  = DB::table('carts')->where('customer_id', Auth::guard('customer')->user()->id)->pluck('product_id');
+            $cartProductsArray = $cartProducts->toArray();
+        } else {
+            $wishlistProductsArray = [];
+            $cartProductsArray = [];
+        }
+        return view(VIEW_FILE_NAMES['my-cart-added'], (compact('wishlistProductsArray','products', 'home_categories')));
     }
     public function add_payment()
     {
