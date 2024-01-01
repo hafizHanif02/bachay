e64f4e5e2b3b9894c936e541394485a5a349b58d
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-01 11:21:28 +0500

    coming soon

--- resources/themes/default/layouts/front-end/sub-category/section5.blade.php
+++ resources/themes/default/layouts/front-end/sub-category/section5.blade.php
@@ -1,19 +1,22 @@
-<div class="container-xxl row mt-5 pb-5 col-12 flash-sales-container d-flex justify-content-between ">
-    @foreach ($categories->take(4) as $category)
-        <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <a href="#" class="text-decoration-none">
-                <div class="ImageCOntainer card rounded-circle for-border1">
-                    <img class="card-img rounded-circle"
-                        src="{{ asset('storage/app/public/category/' . $category->icon) }}" alt="Flash Sale 1"
-                        width="100%" height="100%" />
-                </div>
-                <h4 class="text-center text-dark">{{ $category->name }}</h4>
-            </a>
-        </div>
-    @endforeach
-
-
-
-
+<div class="container-xxl">
+    <div class="row mt-5 pb-5 col-12 flash-sales-container d-flex justify-content-between ">
+        @foreach ($categories->take(4) as $category)
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <a href="#" class="text-decoration-none">
+                    <div class="ImageCOntainer card rounded-circle for-border1">
+                        <img class="card-img rounded-circle"
+                            src="{{ asset('storage/app/public/category/' . $category->icon) }}" alt="Flash Sale 1"
+                            width="100%" height="100%" />
+                    </div>
+                    <h4 class="text-center text-dark">{{ $category->name }}</h4>
+                </a>
+            </div>
+        @endforeach
+    
+    
+    
+    
+    
+    </div>
 
 </div>
