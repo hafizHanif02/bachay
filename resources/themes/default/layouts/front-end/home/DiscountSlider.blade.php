f753a2ace7057f24e16fc86d0f17a23b44ee525e
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-01 18:41:45 +0500

    home category get done

--- resources/themes/default/layouts/front-end/home/DiscountSlider.blade.php
+++ resources/themes/default/layouts/front-end/home/DiscountSlider.blade.php
@@ -3,24 +3,25 @@
         <h1 class="text-center textClr">Flash Sales For Child Products Get Crazy Discounts</h1>
 
         <div class="row mt-5 col-12 flash-sales-container ">
-            @if(count($productsInFlashDeal) > 0)
-            @foreach ($productsInFlashDeal->take(6) as $products)
+            {{-- @foreach ($categories as $category) --}}
+            @if(count($categories) > 0)
+            @foreach ($categories->take(6) as $category)
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-5 mt-4">
                     <div class="card rounded-5">
-                        <a href="{{ route('product-detail', $products->id) }}">
-                            <div class="deal-alert-circle">-{{ $products->discount }}%</div>
+                        <a href="{{ route('product-detail', $category->id) }}">
+                            <div class="deal-alert-circle">-{{ $category->discount }}%</div>
                             <div class="forHeight">
                                 {{-- @foreach (json_decode($products->images) as $key => $photo) --}}
                                 <img class="object-fit-cover card-img rounded-5"
-                                    src="{{ asset("storage/app/public/product/thumbnail/$products->thumbnail") }}  "
+                                    src="{{ asset('storage/app/public/category/' . $category->icon) }}"
                                     alt="Flash Sale" width="100%" height="100%" />
                                 {{-- @endforeach --}}
                             </div>
                             <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
-                                @if (strlen($products->name) <= 20)
-                                    <p class="card-text">{{ $products->name }}</p>
+                                @if (strlen($category->name) <= 20)
+                                    <p class="card-text">{{ $category->name }}</p>
                                 @else
-                                    <p class="card-text"> {{ substr($products->name, 0, 20) }}...</p>
+                                    <p class="card-text"> {{ substr($category->name, 0, 20) }}...</p>
                                 @endif
                             </div>
                         </a>
