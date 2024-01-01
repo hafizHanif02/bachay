bb36580e2efee0ef052c33d8d0c63327cd3dc074
shahoon malik <151923050+Shahoon-Ahmed-Malik@users.noreply.github.com>
2024-01-01 13:19:19 +0500

    sub-cat sec 5 name dynamic added

--- resources/themes/default/layouts/front-end/sub-category/section4.blade.php
+++ resources/themes/default/layouts/front-end/sub-category/section4.blade.php
@@ -1,4 +1,3 @@
-
 @php
     $shuffledProducts = $productsInFlashDeal->shuffle()->take(7);
 @endphp
@@ -7,12 +6,19 @@
     <div class="row col-12 flash-sales-container d-flex justify-content-center">
         @foreach ($shuffledProducts as $products)
             <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                <a href="#">
+                <a href="#" style="text-decoration: none">
                     <div class="ImgCon card rounded-4 for-border">
                         <img class="card-img rounded-4 object-fit-cover"
                             src="{{ asset('storage/app/public/product/thumbnail/' . $products->product->thumbnail) }}"
                             alt="Flash Sale 1" width="100%" height="100%" />
                     </div>
+                    <h4 class="text-center text-dark">
+                        @if (strlen($products->product->name) <= 20)
+                            {{ $products->product->name }}
+                        @else
+                            <span class="card-text"> {{ substr($products->product->name, 0, 20) }}...</span>
+                        @endif
+                    </h4>
                 </a>
             </div>
         @endforeach
