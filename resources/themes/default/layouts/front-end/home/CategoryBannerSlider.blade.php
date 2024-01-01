ba11066e6866d122bf0c5f277b6e281d0aadc165
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-01 10:39:39 +0500

    banner

--- resources/themes/default/layouts/front-end/home/CategoryBannerSlider.blade.php
+++ resources/themes/default/layouts/front-end/home/CategoryBannerSlider.blade.php
@@ -1,5 +1,5 @@
 <div class="sub-banner container-xxl">
-    <img class="rounded-5 img-fluid imgBanner" src="{{ asset('storage/app/public/banner/'.$main_section_banner[0]->photo) }}" alt="banner" width="100%">
+    <img class="rounded-5 img-fluid imgBanner" src="{{ asset('storage/app/public/banner/'.$main_section_banner->photo) }}" alt="banner" width="100%">
 </div> 
 
 
