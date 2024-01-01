b3eacf691524cb1706d554b006001369b855e3db
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-01 17:53:13 +0500

    footer

--- resources/themes/default/layouts/front-end/partials/footer.blade.php
+++ resources/themes/default/layouts/front-end/partials/footer.blade.php
@@ -1,114 +1,117 @@
 <footer>
     <div class="desktop-footer">
-        <div class="row">
-            <div class="col list-group">
-                <ul>
-                    <h2 class="myfont">Company</h2>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Account</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Privacy Policy</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Terms & Conditions</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Contact Us</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Support Center</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>About Us</h5>
-                        </a></li>
-                    <li class="poppins"><a href="" class="text-decoration-none">
-                            <h5>Become A Vendor</h5>
-                        </a></li>
-                </ul>
+        <div class="container-xxl">
+            <div class="row">
+                <div class="col list-group">
+                    <ul>
+                        <h2 class="myfont">Company</h2>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Account</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Privacy Policy</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Terms & Conditions</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Contact Us</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Support Center</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>About Us</h5>
+                            </a></li>
+                        <li class="poppins"><a href="" class="text-decoration-none">
+                                <h5>Become A Vendor</h5>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="col list-group">
+                    <ul>
+                        <h2 class="myfont">Account</h2>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Login</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>View Cart</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>My Wishlist</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Track My Order</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Help Ticket</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Shipping Details</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Compare Products</h5>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="col list-group">
+                    <ul>
+                        <h2 class="myfont">Corporate</h2>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Affiliate Program</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Accessibility</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Our Suppliers</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Promotions</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Features</h5>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="col list-group">
+                    <ul>
+                        <h2 class="myfont">Popular</h2>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Boys Fashion</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Girls Fashion</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>0 - 06 Years Olds</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Entertainment</h5>
+                            </a></li>
+                        <li class="poppins"><a href="#" class="text-decoration-none">
+                                <h5>Parenting</h5>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="col list-group">
+                    <ul>
+                        <li><a href="#" class="text-decoration-none"><img src="{{ asset('public/images/logo.png') }}"
+                                    alt="Logo"></a></li>
+    
+                        <li class="mt-4 poppins"><a href="#" class="text-decoration-none">
+                                <h6><i class="bi bi-geo-alt"></i> Morem ipsum dolor sit amet.</h6>
+                            </a></li>
+                        <li class="poppins mt-3"><a href="tel:" class="text-decoration-none">
+                                <h6><i class="bi bi-telephone"></i> 0000-000-0000 </h6>
+                            </a></li>
+                        <li class="poppins mt-3"><a href="mailto:" class="text-decoration-none">
+                                <h6><i class="bi bi-envelope"></i> bac@company.com</h6>
+                            </a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="col list-group">
-                <ul>
-                    <h2 class="myfont">Account</h2>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Login</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>View Cart</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>My Wishlist</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Track My Order</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Help Ticket</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Shipping Details</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Compare Products</h5>
-                        </a></li>
-                </ul>
-            </div>
-            <div class="col list-group">
-                <ul>
-                    <h2 class="myfont">Corporate</h2>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Affiliate Program</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Accessibility</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Our Suppliers</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Promotions</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Features</h5>
-                        </a></li>
-                </ul>
-            </div>
-            <div class="col list-group">
-                <ul>
-                    <h2 class="myfont">Popular</h2>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Boys Fashion</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Girls Fashion</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>0 - 06 Years Olds</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Entertainment</h5>
-                        </a></li>
-                    <li class="poppins"><a href="#" class="text-decoration-none">
-                            <h5>Parenting</h5>
-                        </a></li>
-                </ul>
-            </div>
-            <div class="col list-group">
-                <ul>
-                    <li><a href="#" class="text-decoration-none"><img src="{{ asset('public/images/logo.png') }}"
-                                alt="Logo"></a></li>
 
-                    <li class="mt-4 poppins"><a href="#" class="text-decoration-none">
-                            <h6><i class="bi bi-geo-alt"></i> Morem ipsum dolor sit amet.</h6>
-                        </a></li>
-                    <li class="poppins mt-3"><a href="tel:" class="text-decoration-none">
-                            <h6><i class="bi bi-telephone"></i> 0000-000-0000 </h6>
-                        </a></li>
-                    <li class="poppins mt-3"><a href="mailto:" class="text-decoration-none">
-                            <h6><i class="bi bi-envelope"></i> bac@company.com</h6>
-                        </a></li>
-                </ul>
-            </div>
         </div>
     </div>
 </footer>
