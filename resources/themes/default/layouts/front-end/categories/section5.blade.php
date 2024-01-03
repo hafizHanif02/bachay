9cf8884f1e1f0cf3cd9e9034058f0593e3b5f5e1
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-03 17:54:01 +0500

    cate dynamic

--- resources/themes/default/layouts/front-end/categories/section5.blade.php
+++ resources/themes/default/layouts/front-end/categories/section5.blade.php
@@ -1,29 +1,72 @@
 <div class="container-xxl premiumBouquets mb-4">
     <h1 class="text-center textClr">Style For Every Kid</h1>
     <div class="row mt-5 col-12 flash-sales-container">
-        @foreach ($productsInFlashDeal->take(6) as $products)
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
-                <div class="card rounded-5">
-                    <a href="{{ route('product-detail', $products->id) }}">
-                        <div class="deal-alert-circle">-{{ $products->discount }}%</div>
-                        <div class="forHeight">
-                            <img class="object-fit-cover card-img rounded-5"
-                                src="{{ asset('storage/app/public/product/thumbnail/' . $products->product->thumbnail) }}"
-                                alt="Flash Sale" width="100%" height="100%" />
-                        </div>
-                        <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
-                            @if (strlen($products->product->name) <= 20)
-                                <p class="card-text">{{ $products->product->name }}</p>
-                            @else
-                                <p class="card-text"> {{ substr($products->product->name, 0, 20) }}...</p>
-                            @endif
-                        </div>
-                    </a>
+        @if (count($categories) > 0)
+            @foreach ($categories->sortByDesc('created_at')->take(6) as $category)
+                <div class="icon col-lg-4 col-md-6 col-sm-12 mb-5 mt-4">
+                    <div class="card card_image rounded-5">
+                        <a href="{{ route('product-detail', $category->id) }}">
+                            <div class="deal-alert-circle">-{{ $category->discount }}%</div>
+                            <div class="forHeight">
+                                <img class="object-fit-cover card-img rounded-5"
+                                    src="{{ asset('storage/app/public/category/' . $category->icon) }}"
+                                    alt="Flash Sale" width="100%" height="100%" />
+                            </div>
+                            <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
+                                @if (strlen($category->name) <= 20)
+                                    <p class="card-text">{{ $category->name }}</p>
+                                @else
+                                    <p class="card-text"> {{ substr($category->name, 0, 20) }}...</p>
+                                @endif
+                            </div>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-
-
+            @endforeach
+        @endif
     </div>
 
 </div>
+<style>
+    .bgcolor {
+        background: #fbdabf;
+    }
+    .card_image {
+        z-index: -1 !important;
+    }
+    .ruler {
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        background-color: rgba(255, 255, 255, 0.05);
+        position: absolute;
+        top: 50%;
+        height: 50%;
+        left: 0%;
+        right: 0%;
+    }
+    .icon {
+        --transition-duration: 500ms;
+        --transition-easing: ease-out;
+        backdrop-filter: blur(2px);
+        transition: transform var(--transition-duration) var(--transition-easing);
+        overflow: hidden;
+        &::before {
+            content: '';
+            background: rgba(255, 255, 255, 0.4);
+            width: 20%;
+            height: 100%;
+            top: 0%;
+            left: -125%;
+            transform: skew(45deg);
+            position: absolute;
+            transition: left var(--transition-duration) var(--transition-easing);
+        }
+
+        &:hover {
+            transform: translateY(-4%);
+
+            &::before {
+                left: 150%;
+            }
+        }
+    }
+</style>
