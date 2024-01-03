a97c50f3ad1f58e2f8bb92183f15409ff366d63d
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-03 17:56:23 +0500

    sub banner

--- resources/themes/default/layouts/front-end/categories/section3.blade.php
+++ resources/themes/default/layouts/front-end/categories/section3.blade.php
@@ -1,3 +1,5 @@
-<div class="container-xxl sub-banner">
-    <img class="rounded-5 img-fluid imgBanner" src="{{ asset('storage/app/public/banner/'.$main_section_banner[0]->photo) }}" alt="banner" width="100%">
-</div> 
+<div class="sub-banner container-xxl">
+    @if (count($main_section_banner) > 0 && $main_section_banner)
+        <img class="rounded-5 imgBanner object-fit-cover" src="{{ asset('storage/app/public/banner/'.$main_section_banner[0]->photo) }}" alt="banner" width="100%" height="280px">
+    @endif
+</div>
