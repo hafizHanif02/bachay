2f14989dd7b8cfc0e058468033e2b758a00b89ab
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-04 14:47:55 +0500

    some fix

--- resources/views/naveedyousufcard.blade.php
+++ resources/views/naveedyousufcard.blade.php
@@ -11,8 +11,8 @@
     }
 
     .flip-card-inner {
-      width: 100%;
-      height: 100%;
+      /* width: 100%;
+      height: 100%; */
       transform-style: preserve-3d;
       transition: transform 0.6s;
     }
@@ -23,8 +23,8 @@
 
     .flip-card-front,
     .flip-card-back {
-      width: 100%;
-      height: 100%;
+      /* width: 100%;
+      height: 100%; */
       position: absolute;
       backface-visibility: hidden;
     }
