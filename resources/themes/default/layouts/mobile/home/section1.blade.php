87233c9b80c05f2802013d80279ed49f8508f6dc
shahoon malik <151923050+Shahoon-Ahmed-Malik@users.noreply.github.com>
2024-01-08 18:28:57 +0500

    mobile sec 1,2 dynamic

--- resources/themes/default/layouts/mobile/home/section1.blade.php
+++ resources/themes/default/layouts/mobile/home/section1.blade.php
@@ -1,11 +1,20 @@
 <div class="slider-nav slider-con mt-1">
-    <div class="">
-        <a href="#" class="text-decoration-none">
-            <img class="New-Season rounded-4 new-arival-container" src="{{ asset('public/images/child.png') }}" alt="">
-            <h3 class="text-center gradient-text fs-12">New Season</h3>
-        </a>
-    </div>
-    <div class="">
+    @foreach ($categories as $category)
+        <div class="">
+            <a href="#" class="text-decoration-none">
+                <img class="New-Season rounded-4 new-arival-container"
+                    src="{{ asset('storage/app/public/category') }}/{{ $category['icon'] }}" alt="">
+                <h3 class="text-center gradient-text fs-12">
+                    @if (strlen($category->name) <= 15)
+                        {{ $category->name }}
+                    @else
+                        {{ substr($category->name, 0, 15) }}<span id="dots">...</span>
+                    @endif
+                </h3>
+            </a>
+        </div>
+    @endforeach
+    {{-- <div class="">
         <a href="#" class="text-decoration-none">
             <img class="Gifts rounded-4 new-arival-container" src="{{ asset('public/images/Frame22.png') }}" alt="">
             <h3 class="text-center text-dark fs-12">Gifts <span class="text-success">-15%</span></h3>
@@ -28,5 +37,5 @@
             <img class="Education rounded-4 new-arival-container" src="{{ asset('public/images/books.png') }}" alt="">
             <h3 class="text-center text-dark fs-12">Education</h3>
         </a>
-    </div>              
+    </div>               --}}
 </div>
--- resources/themes/default/layouts/mobile/home/section2.blade.php
+++ resources/themes/default/layouts/mobile/home/section2.blade.php
@@ -1,8 +1,15 @@
 <div class="slider-container mobileSection-main mt-3">
     <div class="slider-main">
-        <div><img class="rounded-5 img-fluid imgBanner"  src="{{ asset('public/images/image 5.png') }}" alt="Image 1"></div>
-        <div><img class="rounded-5 img-fluid imgBanner"  src="{{ asset('public/images/image 5.png') }}" alt="Image 2"></div>
-        <div><img class="rounded-5 img-fluid imgBanner"  src="{{ asset('public/images/image 5.png') }}" alt="Image 3"></div>
+        @foreach ($main_banner as $banner)
+            <div>
+                <img class="rounded-5 img-fluid imgBanner"
+                    src="{{ asset('storage/app/public/banner/' . $banner->photo) }}" alt="Image">
+            </div>
+        @endforeach
+
+        {{-- <div><img class="rounded-5 img-fluid imgBanner"  src="{{ asset('public/images/image 5.png') }}" alt="Image 2"></div>
+        <div><img class="rounded-5 img-fluid imgBanner"  src="{{ asset('public/images/image 5.png') }}" alt="Image 3"></div> --}}
         <!-- Add more slides as needed -->
     </div>
 </div>
+
