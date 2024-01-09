66d66297e9e202a05a2f2afa5b1d67a596408cc9
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-09 12:19:33 +0500

    category dynamic done mobile

--- resources/themes/default/layouts/mobile/categories/all-categories.blade.php
+++ resources/themes/default/layouts/mobile/categories/all-categories.blade.php
@@ -1,189 +1,24 @@
 <div class="mobileContainer space-between">
     <div class="row col-12 mt-3 mb-5">
-
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="New-Season1 rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img1.png') }}" alt="">
-                </div>
-
-                <h3 class="text-center text-dark fs-9">Art & Hobbies</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Gifts rounded-circle new-arival-container" src="{{ asset('public/images/cate-img2.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Gifts <span class="text-success">-15%</span></h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Kids-Accessories rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img3.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Nursing</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Education rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img4.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Education</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img2.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Health & Safety</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img1.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Maternity</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Entertainment</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img2.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Footwears</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Books</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img1.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img2.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Maternity</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img1.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Maternity</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img2.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Nursing</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container" src="{{ asset('public/images/kid.png') }}"
-                        alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Fashion</h3>
-            </a>
-        </div>
-        <div class="col-3 mt-2">
-            <a href="#" class="text-decoration-none">
-                <div class="for-sizing">
-                    <img class="Fashion rounded-circle new-arival-container"
-                        src="{{ asset('public/images/cate-img2.png') }}" alt="">
-                </div>
-                <h3 class="text-center text-dark fs-9">Nursing</h3>
-            </a>
-        </div>
+        @foreach ($categories->SortByDesc('created_at') as $category)
+            <div class="col-3 mt-2">
+                <a href="#" class="text-decoration-none">
+                    <div class="for-sizing">
+                        <img class="Gifts rounded-circle new-arival-container"
+                            src="{{ asset('storage/app/public/category') }}/{{ $category['icon'] }}" alt="">
+                    </div>
+                    <h3 class="text-center text-dark fs-9">
+                        @if (strlen($category->name) <= 15)
+                            {{ $category->name }}
+                        @else
+                            {{ substr($category->name, 0, 15) }}<span id="dots">...</span>
+                        @endif
 
 
+                    </h3>
+                </a>
+            </div>
+        @endforeach
     </div>
 
 
