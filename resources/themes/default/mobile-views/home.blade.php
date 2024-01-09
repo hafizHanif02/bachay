468bdafc39f1b7036390a2fb7c0599310c59b0cd
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-09 16:17:15 +0500

    working on product page

--- resources/themes/default/mobile-views/home.blade.php
+++ resources/themes/default/mobile-views/home.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.front-end.mobile-layout')
 
 @section('content')
-   
-    @foreach ($home_layouts as $layout)
+@include('layouts.mobile.home.section1')
+@include('layouts.mobile.home.section2')
+@include('layouts.mobile.home.section3')
+@include('layouts.mobile.home.section4')
+@include('layouts.mobile.home.section5')
+@include('layouts.mobile.home.section6')
+@include('layouts.mobile.home.section7')
+@include('layouts.mobile.home.section8')
+@include('layouts.mobile.home.section9')
+@include('layouts.mobile.home.section10')
+@include('layouts.mobile.home.section11')
+@include('layouts.mobile.home.section12')
+@include('layouts.mobile.home.section13')
+@include('layouts.mobile.home.section14')
+
+
+    {{-- @foreach ($home_layouts as $layout)
         @includeIf('layouts.mobile.home.' . $layout->section_name)
-    @endforeach
+    @endforeach --}}
 @endsection
---
+++ resources/themes/default/mobile-views/products.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front-end.mobile-layout')
+
+@section('content')
+    @include('layouts.mobile.products.category-buttons')
+    @include('layouts.mobile.products.full-width-banner')
+    @include('layouts.mobile.products.products-slider')
+    @include('layouts.mobile.products.products-cards')
+   
+
+@endsection
