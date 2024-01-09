fe22f660d0356677a9080035fb5550d739799de1
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-01-09 11:02:27 +0500

    working on home layout mobile

--- resources/themes/default/layouts/front-end/mobile-layout.blade.php
+++ resources/themes/default/layouts/front-end/mobile-layout.blade.php
@@ -135,20 +135,9 @@
         </form>
     </header>
     <main>
-    @include('layouts.mobile.home.section1')
-    @include('layouts.mobile.home.section2')
-    @include('layouts.mobile.home.section3')
-    @include('layouts.mobile.home.section4')
-    @include('layouts.mobile.home.section5')
-    @include('layouts.mobile.home.section6')
-    @include('layouts.mobile.home.section7')
-    @include('layouts.mobile.home.section8')
-    @include('layouts.mobile.home.section9')
-    @include('layouts.mobile.home.section10')
-    @include('layouts.mobile.home.section11')
-    @include('layouts.mobile.home.section12')
-    @include('layouts.mobile.home.section13')
-    @include('layouts.mobile.home.section14')
+  
+    @yield('content')
+
   
     </main>
     <footer>
