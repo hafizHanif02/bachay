4d0e8d185335d089b98f674c5527f089c2b57154
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-01-17 16:40:37 +0500

    Update ProductSlider.blade.php

--- resources/themes/default/layouts/front-end/home/ProductSlider.blade.php
+++ resources/themes/default/layouts/front-end/home/ProductSlider.blade.php
@@ -1,4 +1,4 @@
-<div class="sub-contain container-xxl">
+<div class="sub-contain container-fluid">
     <div class="row mb-4">
         <div class="col-12 d-flex justify-content-between align-items-center">
             <h1 class="textClr m-0">Autumn Whisper</h1>
