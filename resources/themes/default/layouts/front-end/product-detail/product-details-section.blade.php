4b675a0ec0cb8fce23c4709315c30b249d415db3
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-01-24 07:33:07 -0700

    Update product-details-section.blade.php

--- resources/themes/default/layouts/front-end/product-detail/product-details-section.blade.php
+++ resources/themes/default/layouts/front-end/product-detail/product-details-section.blade.php
@@ -1,8 +1,35 @@
+<link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'>
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css'>
+
+
 <div class="container-fluid products mt-4">
     <div class="row">
 
         {{-- {{ $prod }} --}}
 
+<!-- default start -->
+<section id="default" class="padding-top0">
+    <div class="row">
+      <div class="large-12 column"><h3>Product Zooming</h3></div>
+        <hr>
+      <div class="large-5 column">
+        <div class="xzoom-container">
+          <img class="xzoom" id="xzoom-fancy" src="path/to/image.jpg" xoriginal="path/to/full-image.jpg" />
+          <div class="xzoom-thumbs">
+            <a href="path/to/image.jpg"><img class="xzoom-gallery" width="80" src="path/to/image.jpg"  xpreview="path/to/image.jpg" title="The description goes here"></a>
+
+            <a href="path/to/image.jpg"><img class="xzoom-gallery" width="80" src="path/to/image.jpg" title="The description goes here"></a>
+
+            <a href="path/to/image.jpg" title="The description goes here"></a>
+
+            <a href="path/to/image.jpg"><img class="xzoom-gallery" width="80" src="path/to/image.jpg" title="The description goes here"></a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    </section>
+    <!-- default end -->
 
         <div class="col-lg-6 col-md-6 col-sm-12 col-12 d-flex flex-column fixedProduct">
             <div class="product-container">
@@ -30,7 +57,7 @@
 
 
                 {{-- <div class="small-images">
-                        <div class="SmallImageCon">     
+                        <div class="SmallImageCon">
                                 @foreach (json_decode($product->color_image) as $key => $photo)
                                     <img class="small-image object-fit-cover"
                                         id="image-#{{ $photo->color ? $photo->color : '' }}"
@@ -652,11 +679,21 @@
     .form-group-inline {
         width: 49%;
     }
-    
+
     .border-ww{
         border: 2px solid #000;
     }
+    #default{
+  text-align:center;
+}
 </style>
+
+<script src='https://code.jquery.com/jquery-2.1.1.js'></script>
+<script src='https://unpkg.com/xzoom/dist/xzoom.min.js'></script>
+<script src='https://hammerjs.github.io/dist/hammer.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js'></script>
+
 <script>
    $(".slider").slick({
     autoplay: true,
@@ -718,4 +755,155 @@
 
 
     }
+
+    (function ($) {
+    $(document).ready(function() {
+        $('.xzoom, .xzoom-gallery').xzoom({zoomWidth: 400, title: true, tint: '#333', Xoffset: 15});
+        $('.xzoom2, .xzoom-gallery2').xzoom({position: '#xzoom2-id', tint: '#ffa200'});
+        $('.xzoom3, .xzoom-gallery3').xzoom({position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden'});
+        $('.xzoom4, .xzoom-gallery4').xzoom({tint: '#006699', Xoffset: 15});
+        $('.xzoom5, .xzoom-gallery5').xzoom({tint: '#006699', Xoffset: 15});
+
+        //Integration with hammer.js
+        var isTouchSupported = 'ontouchstart' in window;
+
+        if (isTouchSupported) {
+            //If touch device
+            $('.xzoom, .xzoom2, .xzoom3, .xzoom4, .xzoom5').each(function(){
+                var xzoom = $(this).data('xzoom');
+                xzoom.eventunbind();
+            });
+
+            $('.xzoom, .xzoom2, .xzoom3').each(function() {
+                var xzoom = $(this).data('xzoom');
+                $(this).hammer().on("tap", function(event) {
+                    event.pageX = event.gesture.center.pageX;
+                    event.pageY = event.gesture.center.pageY;
+                    var s = 1, ls;
+
+                    xzoom.eventmove = function(element) {
+                        element.hammer().on('drag', function(event) {
+                            event.pageX = event.gesture.center.pageX;
+                            event.pageY = event.gesture.center.pageY;
+                            xzoom.movezoom(event);
+                            event.gesture.preventDefault();
+                        });
+                    }
+
+                    xzoom.eventleave = function(element) {
+                        element.hammer().on('tap', function(event) {
+                            xzoom.closezoom();
+                        });
+                    }
+                    xzoom.openzoom(event);
+                });
+            });
+
+        $('.xzoom4').each(function() {
+            var xzoom = $(this).data('xzoom');
+            $(this).hammer().on("tap", function(event) {
+                event.pageX = event.gesture.center.pageX;
+                event.pageY = event.gesture.center.pageY;
+                var s = 1, ls;
+
+                xzoom.eventmove = function(element) {
+                    element.hammer().on('drag', function(event) {
+                        event.pageX = event.gesture.center.pageX;
+                        event.pageY = event.gesture.center.pageY;
+                        xzoom.movezoom(event);
+                        event.gesture.preventDefault();
+                    });
+                }
+
+                var counter = 0;
+                xzoom.eventclick = function(element) {
+                    element.hammer().on('tap', function() {
+                        counter++;
+                        if (counter == 1) setTimeout(openfancy,300);
+                        event.gesture.preventDefault();
+                    });
+                }
+
+                function openfancy() {
+                    if (counter == 2) {
+                        xzoom.closezoom();
+                        $.fancybox.open(xzoom.gallery().cgallery);
+                    } else {
+                        xzoom.closezoom();
+                    }
+                    counter = 0;
+                }
+            xzoom.openzoom(event);
+            });
+        });
+
+        $('.xzoom5').each(function() {
+            var xzoom = $(this).data('xzoom');
+            $(this).hammer().on("tap", function(event) {
+                event.pageX = event.gesture.center.pageX;
+                event.pageY = event.gesture.center.pageY;
+                var s = 1, ls;
+
+                xzoom.eventmove = function(element) {
+                    element.hammer().on('drag', function(event) {
+                        event.pageX = event.gesture.center.pageX;
+                        event.pageY = event.gesture.center.pageY;
+                        xzoom.movezoom(event);
+                        event.gesture.preventDefault();
+                    });
+                }
+
+                var counter = 0;
+                xzoom.eventclick = function(element) {
+                    element.hammer().on('tap', function() {
+                        counter++;
+                        if (counter == 1) setTimeout(openmagnific,300);
+                        event.gesture.preventDefault();
+                    });
+                }
+
+                function openmagnific() {
+                    if (counter == 2) {
+                        xzoom.closezoom();
+                        var gallery = xzoom.gallery().cgallery;
+                        var i, images = new Array();
+                        for (i in gallery) {
+                            images[i] = {src: gallery[i]};
+                        }
+                        $.magnificPopup.open({items: images, type:'image', gallery: {enabled: true}});
+                    } else {
+                        xzoom.closezoom();
+                    }
+                    counter = 0;
+                }
+                xzoom.openzoom(event);
+            });
+        });
+
+        } else {
+            //If not touch device
+
+            //Integration with fancybox plugin
+            $('#xzoom-fancy').bind('click', function(event) {
+                var xzoom = $(this).data('xzoom');
+                xzoom.closezoom();
+                $.fancybox.open(xzoom.gallery().cgallery, {padding: 0, helpers: {overlay: {locked: false}}});
+                event.preventDefault();
+            });
+
+            //Integration with magnific popup plugin
+            $('#xzoom-magnific').bind('click', function(event) {
+                var xzoom = $(this).data('xzoom');
+                xzoom.closezoom();
+                var gallery = xzoom.gallery().cgallery;
+                var i, images = new Array();
+                for (i in gallery) {
+                    images[i] = {src: gallery[i]};
+                }
+                $.magnificPopup.open({items: images, type:'image', gallery: {enabled: true}});
+                event.preventDefault();
+            });
+        }
+    });
+})(jQuery);
 </script>
