c85b5d00c9d1f8a2d93cae3f0956c9569a608a6a
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-01-25 04:56:32 -0700

    Update products.blade.php

--- resources/themes/default/mobile-views/products.blade.php
+++ resources/themes/default/mobile-views/products.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.front-end.mobile-layout')
 
 @section('content')
-    @include('layouts.mobile.products.category-buttons')
-    @include('layouts.mobile.products.full-width-banner')
-    @include('layouts.mobile.products.products-slider')
-    @include('layouts.mobile.products.products-cards')
-   
+    @include('layouts.mobile.product.category-buttons')
+    @include('layouts.mobile.product.full-width-banner')
+    @include('layouts.mobile.product.products-slider')
+    @include('layouts.mobile.product.products-cards')
+
 
 @endsection
