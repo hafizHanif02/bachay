f39bac9a5cfc2d9d94b3d42ac5e1f469efb336ff
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-01-25 04:52:20 -0700

    Update add-to-cart.blade.php

--- resources/themes/default/mobile-views/add-to-cart.blade.php
+++ resources/themes/default/mobile-views/add-to-cart.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.front-end.mobile-layout')
 
 @section('content')
-    @include('layouts.mobile.add-to-cart.section1')
+    @include('layouts.mobile.add-to-cart.product-thumbnail')
+    @include('layouts.mobile.add-to-cart.products-cards')
+    @include('layouts.mobile.add-to-cart.section4')
+    @include('layouts.mobile.add-to-cart.section9')
 @endsection
