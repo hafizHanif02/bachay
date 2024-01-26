736b0f15a758054ee32ec6c575beaa9f25c4db31
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-01-26 03:54:12 -0700

    Update HomeController.php

--- app/Http/Controllers/api/v1/Customer/CMS/HomeController.php
+++ app/Http/Controllers/api/v1/Customer/CMS/HomeController.php
@@ -50,7 +50,7 @@ class HomeController extends Controller
             $child = familyRelation::where('id',$request->id)->first();
             if($child != null){
                 $child->tag = ($child->gender == 1)?'Boy':'Girl';
-                $topArrivalCategories = DB::table('categories')
+                $toparrivalcategorys = DB::table('categories')
                 ->where('parent_id', 0)
                 ->where('priority', '!=', 0)
                 ->whereNotIn('id', function($query) {
@@ -75,7 +75,7 @@ class HomeController extends Controller
                 return response()->json(['message'=>'Child not found.'], 200);
             }
         }else{
-            $topArrivalCategories = DB::table('categories')
+            $toparrivalcategorys = DB::table('categories')
             ->where('parent_id', 0)
             ->where('priority', '!=', 0)
             ->whereNotIn('id', function($query) {
