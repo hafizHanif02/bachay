e23d4f58cfbb9cdcfc0d3e560ac3f9c1bac33e40
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-02-02 11:19:21 +0500

    Update product-list.blade.php

--- resources/themes/default/layouts/front-end/product/product-list.blade.php
+++ resources/themes/default/layouts/front-end/product/product-list.blade.php
@@ -106,25 +106,12 @@
                                 @if ($product->reviews->isEmpty())
                                     <p class="m-0 mt-1">No reviews yet</p>
                                 @else
-                                    @php $averageRating = $product->reviews->avg('rating'); @endphp
-                                    {{ $averageRating }};
-                                    {{-- @for ($i = 0; $i < 1; $i++)
-                                        @if ($i < floor($averageRating))
-                                            <span class="bi bi-star-fill me-2" style="color: #ffc107;"></span>
-                                        @elseif ($i == floor($averageRating) && $averageRating - $i >= 0.5)
-                                            <span class="bi bi-star-half me-2" style="color: #ffc107;"></span>
-                                        @else
-                                            <span class="bi bi-star me-2" style="color: #ffc107;"></span>
-                                        @endif
-                                    @endfor --}}
-                                    {{-- <span class="Reviews">({{ $product->reviews_count }}
-                                         @if($product->reviews_count == 1) Review @else Reviews @endif)
-                                        </span> --}}
-                                        <span class="Reviews">({{ $product->reviews_count }})
-                                            
-                                           </span>
+                                    <span class="bi bi-star-fill me-2" style="color: #ffc107;"></span>
+                                    <span>{{ $averageRating }}.0</span>
+                                    <span class="Reviews">({{ $product->reviews_count }} @if($product->reviews_count == 1) Review @else Reviews @endif)</span>
                                 @endif
                             </div>
+                            
                             
                         
                             <a href="#" class="delivery-btn">Standard Delivery</a>
