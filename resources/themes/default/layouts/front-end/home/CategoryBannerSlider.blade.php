f271a3924fc4829a1a5b60435f558999f6749072
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-02-02 15:29:15 +0500

    home

--- resources/themes/default/layouts/front-end/home/CategoryBannerSlider.blade.php
+++ resources/themes/default/layouts/front-end/home/CategoryBannerSlider.blade.php
@@ -1,5 +1,5 @@
 
-<div class="sub-banner container-xxl">
+<div class="sub-banner container-xxl pb-4">
     @if (count($main_section_banner) > 0 && $main_section_banner)
         <img class="rounded-5 imgBanner object-fit-cover" src="{{ asset('storage/app/public/banner/'.$main_section_banner[0]->photo) }}" alt="banner" width="100%" height="280px">
     @endif
